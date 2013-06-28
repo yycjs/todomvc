fd200c548b5786f2f7936ec68c62031288898676
Eric Kryski <e.kryski@gmail.com>
2013-06-28 10:49:16 -0600

    committing dave's stuff

--- public/js/models/todo.js
+++ public/js/models/todo.js
@@ -4,8 +4,26 @@
 
 	// Basic Todo entry model
 	// { text: 'todo', complete: false }
-	var Todo = can.Model.LocalStorage({
-		storageName: 'todos-canjs'
+	var Todo = can.Model({
+		findAll: function(params) {
+			var deferred = can.Deferred();
+			socket.emit('todo::findAll', function(data) {
+		  	deferred.resolve(data);
+		  });
+		  return deferred;
+		},
+		create: function(data) {
+			socket.emit('todo::create', data);
+			var deferred = can.Deferred();
+			deferred.resolve(data);
+			return deferred;
+		},
+		update: function(id, data) {
+			var deferred = can.Deferred();
+			socket.emit('todo::update', data);
+			deferred.resolve(data);
+			return deferred;
+		}
 	}, {
 		// Returns if this instance matches a given filter
 		// (currently `active` and `complete`)
@@ -14,6 +32,16 @@
 			return !filter || (filter === 'active' && !this.attr('complete')) ||
 				(filter === 'completed' && this.attr('complete'));
 		}
+	});
+
+	window.socket.on('todo created', function(todoData) {
+		console.log('Triggering todo create', todoData);
+		can.trigger(Todo, 'created', new Todo(todoData));
+	});
+
+	window.socket.on('todo updated', function(todoData) {
+		console.log('Triggering todo update', todoData);
+		can.trigger(Todo, 'updated', Todo.model(todoData));
 	});
 
 	// List for Todos
